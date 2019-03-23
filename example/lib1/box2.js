6816031bd68d68ed6dfdb5232b503c17cb7fa69a
Daniel Walasek <lysander6@gmail.com>
2019-03-23 13:02:32 +0100

    example files

---
+++ example/lib1/box2.js
@@ -0,0 +1,35 @@
+define([], function() {
+
+    return {
+        renderBox2: renderBox2,
+        submodule: submodule
+    };
+
+    function renderBox2(node) {
+
+        console.log(node, "renderBox2");
+
+        addEvent(node, 'click', function() {
+
+            console.info("kliknięto, odpalam zagnieżdżony moduł");
+            require.runnerBox.runElement(node);
+        });
+    }
+
+    /*
+    require.runnerBox.runElement(document.querySelectorAll('*[data-run-module="lib2/box2.renderBox2"]')[0])
+    require.runnerBox.runElement(document)
+    */
+    function submodule(domElement) {
+        console.info("IIIII odpaliłem zagnieżdżony element", domElement);
+    }
+
+    function addEvent(element, event, callback) {
+
+        if (element.addEventListener) {
+            element.addEventListener(event, callback, false);
+        } else {
+            element.attachEvent('on' + event, callback);
+        }
+    }
+});
---
+++ example/lib1/mod1.js
@@ -0,0 +1,15 @@
+define([], function() {
+
+    return {
+        "fun1": fun1,
+        "fun2": fun2
+    };
+
+    function fun1() {
+
+    }
+
+    function fun2() {
+
+    }
+});
